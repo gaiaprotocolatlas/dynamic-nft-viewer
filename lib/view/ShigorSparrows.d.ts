ce5bd9d3638fbabdbc5fa0be76c74d1cb6a7ac0f
Sim Young Jae <hanul@hanul.me>
2023-06-07 13:23:16 +0900

    dev

--- lib/view/ShigorSparrows.d.ts
+++ lib/view/ShigorSparrows.d.ts
@@ -1,7 +1,9 @@
 import { View, ViewParams } from "gaia-commons-ts";
 export default class ShigorSparrows extends View {
-    private container;
+    private screen;
     constructor(params: ViewParams);
+    changeParams(params: ViewParams, uri: string): void;
+    private load;
     close(): void;
 }
 //# sourceMappingURL=ShigorSparrows.d.ts.map
--- src/view/ShigorSparrows.ts
+++ src/view/ShigorSparrows.ts
@@ -1,19 +1,66 @@
-import { BodyNode, DomNode, el } from "gaia-commons-browser";
 import { View, ViewParams } from "gaia-commons-ts";
+import { Fullscreen, ImageNode } from "gaia-engine";
+import Config from "../Config.js";
+import ShigorSparrowMetadata from "../metadata/ShigorSparrowMetadata.js";
+import ShigorSparrowsParts from "../metadata/shigor-sparrows-parts.json" assert { type: "json" };
 
 export default class ShigorSparrows extends View {
 
-    private container: DomNode;
+    private screen: Fullscreen;
 
     constructor(params: ViewParams) {
         super();
-        BodyNode.append(this.container = el(".nft-view",
+        this.screen = new Fullscreen(1000, 1000);
 
-        ));
+        if (params.id !== undefined) {
+            this.load(parseInt(params.id));
+        }
+    }
+
+    public changeParams(params: ViewParams, uri: string): void {
+        if (params.id !== undefined) {
+            this.load(parseInt(params.id));
+        }
+    }
+
+    private async load(id: number) {
+
+        const result = await fetch(`https://${Config.appServerHost}/metadata/shigor-sparrows/${id}`);
+        const metadata: ShigorSparrowMetadata = await result.json();
+
+        const imageParts: any[] = [];
+        for (const [traitId, trait] of ShigorSparrowsParts.entries()) {
+            if (
+                trait.condition === undefined ||
+                Object.entries(metadata.parts).find((p) => p[0] === trait.condition.trait && trait.condition.values.includes(p[1]) === true) !== undefined
+            ) {
+                for (const [partId, part] of trait.parts.entries()) {
+                    if (
+                        (part as any).condition === undefined ||
+                        Object.entries(metadata.parts).find((p) => p[0] === (part as any).condition.trait && (part as any).condition.values.includes(p[1]) === true) !== undefined
+                    ) {
+                        if (metadata.parts[trait.name] === part.name) {
+                            imageParts.push({ traitId, partId });
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        let images: any[] = [];
+        for (const imagePart of imageParts) {
+            images = images.concat(ShigorSparrowsParts[imagePart.traitId].parts[imagePart.partId].images);
+        }
+        images.sort((a, b) => a.order - b.order);
+
+        for (const image of images) {
+            new ImageNode(0, 0, `/nft-image-parts/shigor-sparrows/${image.path}`).appendTo(this.screen.root);
+        }
     }
 
     public close(): void {
-        this.container.delete();
+        this.screen.delete();
         super.close();
     }
 }
