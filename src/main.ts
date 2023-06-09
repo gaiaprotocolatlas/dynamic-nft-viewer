8dbf0cdd777428022c165b58526568184b78d319
Sim Young Jae <hanul@hanul.me>
2023-06-09 23:54:12 +0900

    dev

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import GaiaProtocolGods from "./view/GaiaProtocolGods.js";
 import ShigorSparrows from "./view/ShigorSparrows.js";
 
 (async () => {
+
+    const urlParams = new URLSearchParams(location.search);
+    if (urlParams.has("collection") && urlParams.has("tokenId")) {
+        Router.goNoHistory(`${urlParams.get("collection")}/${urlParams.get("tokenId")}`);
+    }
+
     Router.route("gaia-protocol-gods/{id}", GaiaProtocolGods);
     Router.route("shigor-sparrows/{id}", ShigorSparrows);
 })();
