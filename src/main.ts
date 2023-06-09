c0689ab4debf68e315221b15166f822587085dd0
Sim Young Jae <hanul@hanul.me>
2023-06-09 22:33:46 +0900

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
-import { BodyNode, Router, ThemeManager } from "gaia-commons-browser";
+import { Router } from "gaia-commons-browser";
 import GaiaProtocolGods from "./view/GaiaProtocolGods.js";
 import ShigorSparrows from "./view/ShigorSparrows.js";
 
 (async () => {
-    ThemeManager.init();
-
-    if (sessionStorage.__spa_path) {
-        Router.goNoHistory(sessionStorage.__spa_path);
-        sessionStorage.removeItem("__spa_path");
-    }
-    
     Router.route("gaia-protocol-gods/{id}", GaiaProtocolGods);
     Router.route("shigor-sparrows/{id}", ShigorSparrows);
 })();
